2064152932b34473dbfd76d22b3522c9a75f8f70
Cileb <Cileb@users.noreply.github.com>
2022-11-01 20:31:06 -0400

    Test Commit 06

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Books } from "./components";
 function App() {
   return (
     <>
+      <div>Hi</div>
       <Routes>
         <Route path="/books" element={<Books></Books>}></Route>
       </Routes>
