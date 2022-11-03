d182b8b87a94ca8e929a103203da0a8476552017
JBarrett991 <60449860+JBarrett991@users.noreply.github.com>
2022-11-03 19:47:04 -0400

    added some db adapter functions

--- db/sizes.js
+++ db/sizes.js
@@ -14,6 +14,32 @@ const createSize = async ({ size }) => {
   return row;
 };
 
+const getSize = async (id) => {
+  try {
+    const {
+      rows: [size],
+    } = await client.query(`SELECT * FROM crusts WHERE id=($1)`, [id]);
+    return size;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteSize = async (id) => {
+  try {
+    const {
+      rows: [size],
+    } = await client.query(`DELETE FROM routines WHERE id=($1) RETURNING *;`, [
+      id,
+    ]);
+    return size;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   createSize,
+  getSize,
+  deleteSize,
 };
