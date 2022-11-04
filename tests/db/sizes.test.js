02e30b79fb3fc9159680f4734c0a95eaee661309
JBarrett991 <60449860+JBarrett991@users.noreply.github.com>
2022-11-04 17:12:25 -0400

    more

--- tests/db/sizes.test.js
+++ tests/db/sizes.test.js
@@ -4,9 +4,9 @@ const { createSize } = require("../../db/sizes");
 
 describe("DB Sizes", () => {
   describe("createSize", () => {
-    it("Creates a size", () => {
-      const size = "extremely large";
-      const testSize = createSize({ size });
+    it("Creates a size", async () => {
+      const testSize = "extremely large";
+      const { size } = await createSize({ size: testSize });
 
       expect(testSize).toEqual(size);
     });
