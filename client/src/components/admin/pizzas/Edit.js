ee4404963d4b0a507e4a62c4bc8755bf1532f4aa
jbugnon <107010227+jbugnon@users.noreply.github.com>
2022-11-29 19:51:13 -0500

    add/edit/delete pizza

--- client/src/components/admin/pizzas/Edit.js
+++ client/src/components/admin/pizzas/Edit.js
@@ -1,43 +1,59 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { fetchPizza, updatePizzaById } from "../../../api";
+import { addToppingToPizza, destroyPizzaToppings, fetchPizza, updatePizzaById } from "../../../api";
+import Select from "react-select";
 
-const Edit = ({ sizes, crusts, user, handleUpdate }) => {
+const Edit = ({ sizes, crusts, user, handleUpdate, toppings }) => {
   const [pizza, setPizza] = useState({});
   const [name, setName] = useState("");
   const [featured, setFeatured] = useState(true);
   const [crustId, setCrustId] = useState(null);
   const [sizeId, setSizeId] = useState(null);
+  const [double, setDouble] = useState(false)
+  const [amount, setAmount] = useState("full")
+  const [selectedOption, setSelectedOption] = useState([])
   const { id } = useParams()
   const navigate = useNavigate()
-  
+
+  let options = toppings.map(el => {
+    return {
+      value: el, label: el.name
+    }
+  })
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     let userId = user.id
-   let pizza = await updatePizzaById(localStorage.getItem("token"), id, { name, crustId, sizeId, userId,featured } )
-   setPizza(pizza)
-   handleUpdate({})
+    await updatePizzaById(localStorage.getItem("token"), id, { name, crustId, sizeId, userId, featured })
+    await destroyPizzaToppings(id,localStorage.getItem("token"))
+
+    selectedOption.forEach(async (el) => {
+      let r = await addToppingToPizza(localStorage.getItem("token"), id, el.value.id, amount, double)
+      console.log(r)
+    })
     navigate("/admin/pizzas")
+    handleUpdate({})
+    
   };
 
 
   const getPizzaById = async () => {
-    console.log(id)
     let res = await fetchPizza(id)
     setPizza(res)
+    setSelectedOption(res.toppings.map(el => options[el.toppingId - 1]))
+
 
   }
-useEffect(() =>{
-  console.log(pizza)
-  setName(pizza.name)
+  useEffect(() => {
+    setName(pizza.name)
     setFeatured(pizza.featured)
     setCrustId(pizza.crustId)
     setSizeId(pizza.sizeId)
-}, [pizza])
+  }, [pizza])
 
   useEffect(() => {
     getPizzaById()
-    
+
   }, [])
 
   return (
@@ -51,7 +67,7 @@ useEffect(() =>{
         <div className="form-data">
           <label>featured:</label>
           <div>
-          
+
             <input onChange={(e) => setFeatured(e.target.value)} type="radio" id="featured" name="featured" value={true} />
             <span>true</span>
             <input onChange={(e) => setFeatured(e.target.value)} type="radio" id="featured" name="featured" value={false} />
@@ -71,6 +87,43 @@ useEffect(() =>{
             {crusts &&
               crusts.map((crust) => <option value={crust.id} key={crust.id}>{crust.name}</option>)}
           </select>
+        </div>
+        <div className="form-data">
+          <label>Toppings</label>
+          <Select
+            isMulti
+            options={options}
+            value={selectedOption}
+            isClearable={true}
+            onChange={(e) => {
+              setSelectedOption(e)
+            }}
+          />
+        </div>
+        <div className="form-data">
+          <label>amount:</label>
+
+          <select
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+            name="amount"
+          >
+            <option selected value="full">Full</option>
+            <option selected value="left">Left</option>
+            <option selected value="right">Right</option>
+
+
+          </select>
+        </div>
+
+        <div className="form-data">
+          <label>double:</label>
+          <div>
+            <input onChange={(e) => setDouble(true)} type="radio" id="double" name="double" value={true} />
+            <span>true</span>
+            <input onChange={(e) => setDouble(false)} type="radio" id="double" name="double" value={false} />
+            <span>false</span>
+          </div>
         </div>
         <button className="add-btn" type="submit">Edit Pizza</button>
         {console.log(pizza)}
